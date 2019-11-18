8ca78ec4d269a30b0e2f98a172b53c45b2ffa75a
tdreyer1 <tdreyer1@gmail.com>
2019-11-17 22:24:50 -0600

    Add warning to save script

--- minecraft/files/save-server.sh
+++ minecraft/files/save-server.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+/usr/local/bin/mcrcon -H 127.0.0.1 -p {{ minecraft.rcon_pass }} "say Saving world, beware of lag..." 
+sleep 2
 /usr/local/bin/mcrcon -H 127.0.0.1 -p {{ minecraft.rcon_pass }} save-all
+/usr/local/bin/mcrcon -H 127.0.0.1 -p {{ minecraft.rcon_pass }} save-off
