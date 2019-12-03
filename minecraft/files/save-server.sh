1c8dafb2be6e144118f284e146c3d65c5533d625
tdreyer1 <tdreyer1@gmail.com>
2019-12-02 20:33:47 -0600

    update script

--- minecraft/files/save-server.sh
+++ minecraft/files/save-server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 /usr/local/bin/mcrcon -H 127.0.0.1 -p {{ minecraft.rcon_pass }} "say Saving world, beware of lag..." 
 sleep 2
-/usr/local/bin/mcrcon -H 127.0.0.1 -p {{ minecraft.rcon_pass }} save-all
 /usr/local/bin/mcrcon -H 127.0.0.1 -p {{ minecraft.rcon_pass }} save-off
+sleep 1
+/usr/local/bin/mcrcon -H 127.0.0.1 -p {{ minecraft.rcon_pass }} save-all
+sleep 1
+/usr/local/bin/mcrcon -H 127.0.0.1 -p {{ minecraft.rcon_pass }} save-on
